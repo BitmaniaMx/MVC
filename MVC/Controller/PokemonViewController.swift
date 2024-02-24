9cb052a5c7782587179d7b46e1161b187cdec58c
rafaelgonzalez <rafael.glez@gmail.com>
2024-02-24 16:32:22 -0600

    Initial Version

--- MVC/Controller/PokemonViewController.swift
+++ MVC/Controller/PokemonViewController.swift
@@ -9,29 +9,59 @@ import UIKit
 
 class PokemonViewController: UIViewController {
     
+    @IBOutlet weak var pokemonTableView: UITableView!
     let pokemonDataManager = PokemonDataManager()
+    var selectedPokemon : Pokemon?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         pokemonDataManager.fetch()
-        print(pokemonDataManager.countPokemons())
-        var i = 0
-        while i < pokemonDataManager.countPokemons(){
-            print(pokemonDataManager.getPokemon(at: i))
-            i += 1
-        }
+    }
+}
+
+
+
+
+extension PokemonViewController : UITableViewDelegate, UITableViewDataSource{
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemonDataManager.countPokemons()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pokemonCell", for: indexPath) as! PokemonCell
+        let pokemon = pokemonDataManager.getPokemon(at: indexPath.row)
+        cell.pokemonImage.image = UIImage(named: pokemon.image)
+        cell.pokemonLabel.text = pokemon.name
+        
+        return cell
     }
-    */
+    
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return "Pokemons"
+//    }
 
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIImageView(image: UIImage(named: "Pokemon"))
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return CGFloat(80.0)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row selected \(indexPath.row)")
+        selectedPokemon = pokemonDataManager.getPokemon(at: indexPath.row)
+        self.performSegue(withIdentifier: "detailSegue", sender: Self.self)
+        
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! PokemonDetailViewController
+        destination.receivedPokemon = selectedPokemon
+    }
+    
 }
